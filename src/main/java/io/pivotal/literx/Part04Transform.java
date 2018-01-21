60747893e36e3a5a070a25f147e7ddfac3b97db3
Neophy Bishnoi <neophyb@gmail.com>
2018-01-21 22:54:30 +0530

    completed Part04Transform

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -13,23 +13,26 @@ public class Part04Transform {
 
 //========================================================================================
 
-	// TODO Capitalize the user username, firstname and lastname
+	//Capitalize the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return mono.map(user -> new User(user.getUsername().toUpperCase(), user.getFirstname().toUpperCase(),
+				user.getLastname().toUpperCase()));
 	}
 
 //========================================================================================
 
-	// TODO Capitalize the users username, firstName and lastName
+	// Capitalize the users username, firstName and lastName
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		// map performs operations synchronously
+		return flux.map(user-> new User(user.getUsername().toUpperCase(), user.getFirstname().toUpperCase(), user.getLastname().toUpperCase()));
 	}
 
 //========================================================================================
 
-	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
+	//Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
 	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+		// flatmap is used when the element is itself a publisher (Moono here)
+		return flux.flatMap(user->asyncCapitalizeUser(user));
 	}
 
 	Mono<User> asyncCapitalizeUser(User u) {
